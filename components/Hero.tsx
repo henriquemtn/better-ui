5d878b5f3bbf4be4010aca0f3dca1143c11997c9
Henrique Silveira <henriquemartinsilveira2@gmail.com>
2024-11-12 23:36:22 -0300

    Add - Hero Animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,18 @@
+'use client'
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function Hero() {
     return (
-        <section className="text-black dark:text-white">
+        <motion.section
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.3, delay: 1, ease: "linear" }}
+            className="text-black dark:text-white">
             <div className="flex flex-col mx-auto max-w-screen-xl px-4 py-12 lg:flex lg:items-center">
                 <span className='border rounded-full z-10 py-0.5 px-6 text-sm text-neutral-900 dark:text-white  flex items-center justify-center gap-1.5 mb-4'>
-                ✨ Free Open Source Tailwind CSS Components
+                    ✨ Free Open Source Tailwind CSS Components
                 </span>
                 <div className="mx-auto max-w-3xl text-center">
 
@@ -36,6 +43,6 @@ export default function Hero() {
                     </div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     )
 }
